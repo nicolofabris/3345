97b2e0351fa8ca2aa2bc370fce9c463d12dc5210
nicolofabris <nicolo.fabriss@gmail.com>
2023-07-06 12:31:29 +0200

    middleware

--- chatty-backend/src/setupServer.ts
+++ chatty-backend/src/setupServer.ts
@@ -1,5 +1,14 @@
 import {Application, json, urlencoded, Response, Request, NextFunction} from 'express'
 import http from 'http'
+import cors from 'cors'
+import helmet from 'helmet'
+import hpp from 'hpp'
+import compressify from 'compression'
+import cookieSession from 'cookie-session'
+import HTTP_STATUS from 'http-status-codes'
+import 'express-async-errors'
+import compression from 'compression'
+
 
 export class ChattyServer {
     private app: Application
@@ -16,9 +25,32 @@ export class ChattyServer {
         this.startServer(this.app)
     }
 
-    private securityMiddleware(app: Application): void {}
+    private securityMiddleware(app: Application): void {
+      app.use(
+        cookieSession({
+            name: 'session',
+            keys: ['test1', 'test2'],
+            maxAge: 24 * 7 * 3600000,
+            secure: false
+        })
+      )
+      app.use(hpp())  
+      app.use(helmet())  
+      app.use(
+        cors({
+            origin: '*',
+            credentials: true,
+            optionsSuccessStatus: 200,
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+        })
+      )
+    }
 
-    private standardMiddleware(app: Application): void {}
+    private standardMiddleware(app: Application): void {
+       app.use(compression()) 
+       app.use(json({limit: '50mb'}))
+       app.use(urlencoded({extended: true, limit: '50mb'}))
+    }
 
     private routeMiddleware(app: Application): void {}
 

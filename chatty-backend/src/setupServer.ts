8c810faaadf4f3c433beb4569e9e4eeb099e80d5
nicolofabris <nicolo.fabriss@gmail.com>
2023-07-10 10:51:36 +0200

    socket io implemented

--- chatty-backend/src/setupServer.ts
+++ chatty-backend/src/setupServer.ts
@@ -6,9 +6,13 @@ import hpp from 'hpp'
 import compressify from 'compression'
 import cookieSession from 'cookie-session'
 import HTTP_STATUS from 'http-status-codes'
+import {Server} from 'socket.io'
+import { createClient } from 'redis'
+import { createAdapter } from '@socket.io/redis-adapter'
 import 'express-async-errors'
 import compression from 'compression'
 import { config } from './config'
+import applicationRoutes from './routes'
 
 const SERVER_PORT = 5000
 
@@ -55,26 +59,46 @@ export class ChattyServer {
        app.use(urlencoded({extended: true, limit: '50mb'}))
     }
 
-    private routeMiddleware(app: Application): void {}
+    private routeMiddleware(app: Application): void {
+      applicationRoutes(app)
+    }
 
     private globalErrorHandler(app: Application): void {}
 
     private async startServer(app: Application): Promise<void> {
       try {
         const httpServer: http.Server = new http.Server(app)
+        const socketIO: Server = await this.createSocketIO(httpServer)
         this.startHttpServer(httpServer)
+        this.socketIOConnections(socketIO)
       } catch (error) {
         console.log(error);
         
       }
     }
 
-    private createSocketIO(httpServer: http.Server): void {}
+    private async createSocketIO(httpServer: http.Server): Promise<Server> {
+      const io: Server = new Server(httpServer, {
+        cors: {
+          origin: config.CLIENT_URL,
+          methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+        }
+      })
+      const pubClient = createClient({url: config.REDIS_HOST})
+      const subClient = pubClient.duplicate()
+      await Promise.all([pubClient.connect(), subClient.connect()])
+      io.adapter(createAdapter(pubClient, subClient))
+      return io
+    }
 
     private startHttpServer(httpServer: http.Server): void {
+      console.log('server has started');
+      
       httpServer.listen(SERVER_PORT, () => {
         console.log('SERVER RUNNING');
         
       })
     }
+
+    private socketIOConnections(io: Server): void {}
 }
